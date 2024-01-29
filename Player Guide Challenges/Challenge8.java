b1d0869c19977867448c87785549ca9d26582dd1
Niels <46904611+NielsvdWetering@users.noreply.github.com>
2024-01-29 11:20:36 +0100

    Update Challenge8.java
    fixed the round off error when base and height would be 1

--- Player Guide Challenges/Challenge8.java
+++ Player Guide Challenges/Challenge8.java
@@ -1,5 +1,9 @@
 import java.util.Scanner;
 
+/*Changed the int variables to double in order to avoid the roundoff error when the base and height are both 1
+ *base and height also needed to be changed to doubles because if they would remain int the calculation would still be rounded of into an int and only after that be assigned to the double area
+ */
+
 public class Challenge8 {
 	public static void main(String[] args) {
 	Scanner sc = new Scanner(System.in);
@@ -8,15 +12,15 @@ public class Challenge8 {
 	//asking for the base of the triangle
 	System.out.print("What is the base of your triangle? ");
 	String baseString = sc.nextLine();
-	int base = Integer.parseInt(baseString);
+	double base = Double.parseDouble(baseString);
 	
 	//asking for the height of the triangle
 	System.out.print("What is the height of the triangle? ");
 	String heightString = sc.nextLine();
-	int height = Integer.parseInt(heightString);
+	double height = Double.parseDouble(heightString);
 	
 	//calculating and printing the area
-	int area = base * height / 2;
+	double area = base * height / 2;
 	System.out.println("the area of the triangle is: " + area);
 	}
 }
