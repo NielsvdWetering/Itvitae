db971f23c875cca2e807ecdedd90feb14a67dbda
Niels <46904611+NielsvdWetering@users.noreply.github.com>
2024-01-31 19:12:04 +0100

    Create Challenge17.java

---
+++ playerguide/Challenge17.java
@@ -0,0 +1,25 @@
+package playerguide;
+
+public class Challenge17 {
+	public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            String fireMode;
+            if (i % 3 != 0 && i % 5 != 0) {
+                fireMode = "Normal";
+            }
+            else if (i % 3 == 0) {
+                if (i % 5 == 0) {
+                    fireMode = "Big Boom";
+                }
+                else {
+                    fireMode = "Fire";
+                }
+            }
+            else {
+                fireMode = "Electic";
+            }
+            
+            System.out.println(i + ". " + fireMode);
+        }
+	}
+}
