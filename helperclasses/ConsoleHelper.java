993c6936347a3ed29c7b5284c15bdca638e640e7
Niels <46904611+NielsvdWetering@users.noreply.github.com>
2024-02-05 14:27:11 +0100

    Update ConsoleHelper.java

--- helperclasses/ConsoleHelper.java
+++ helperclasses/ConsoleHelper.java
@@ -3,14 +3,6 @@ import java.util.Scanner;
 
 public class ConsoleHelper {
 
-    //intInput word op dit moment niet gebruikt in een andere class
-	public static int intInput() {
-        Scanner scan = new Scanner(System.in);
-        String temp = scan.nextLine();
-        int input = Integer.parseInt(temp);
-        return input;
-	}
-    
     public static int askForInteger(String question) {
         Scanner scan = new Scanner(System.in);
         System.out.println(question);
