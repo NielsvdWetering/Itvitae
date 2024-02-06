2c11bec99623ac9eca0b7b97db77c2f409521d26
Niels <46904611+NielsvdWetering@users.noreply.github.com>
2024-02-06 10:34:41 +0100

    Update NoTriples.java

--- shortexercises/NoTriples.java
+++ shortexercises/NoTriples.java
@@ -10,7 +10,6 @@ public class NoTriples {
     
     private static boolean integerNumberArrays(int[] numbers) {
         for (int i = 0; i < (numbers.length - 2); i++) {    //numbers.length -2, because in the if loops it compares to 2 numbers ahead, if this would go to numbers.length the if loop would be out of bounds
-            System.out.println(numbers[++i]);
             if (numbers[i] == numbers[i + 1] && numbers[i] == numbers[i + 2]) return false;
         }
         return true;
