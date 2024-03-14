301b88f5de37dd4bf421130336d8573cf73a918f
Niels <46904611+NielsvdWetering@users.noreply.github.com>
2024-03-14 12:47:34 +0100

    Create FakeLoading.java
    created a helper class to quickly create fake loading bars to add some more fun and flare to other projects

---
+++ helperclasses/FakeLoading.java
@@ -0,0 +1,59 @@
+package helperclasses;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+public class FakeLoading {
+    static Random rnd = new Random();
+
+    public static void fakeLoading(String loadingText, int delayInMilliSeconds, String textAndBackgroundColor) {//1000 milliseconds is 1 second, dus 50 seconden tot 100%
+        String loadingBar = "";
+        for (int percent = 0; percent <=100; percent+=2) {
+            System.out.printf("\r%s%-21s[%-50s]%s%%%s",textAndBackgroundColor, loadingText, loadingBar, percent, TextColor.RESET);
+            loadingBar += "#";
+            try {
+                TimeUnit.MILLISECONDS.sleep(delayInMilliSeconds);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        System.out.println();
+    }
+
+    public static void fakeLoading(String loadingText, int delayInMilliSeconds) {
+        fakeLoading(loadingText, delayInMilliSeconds, "");
+    }
+
+    public static void fakeLoading(String[] loadingTextArray, int[] fakeLoadingTimes, String[] textAndBackgroundColor) {
+        for (int i = 0; i < loadingTextArray.length; i++) {
+            fakeLoading(loadingTextArray[i], fakeLoadingTimes[i], textAndBackgroundColor[i]);
+        }
+    }
+
+    public static void fakeLoading(String[] loadingTextArray, int[] fakeLoadingTimes) {
+        for (int i = 0; i < loadingTextArray.length; i++) {
+            fakeLoading(loadingTextArray[i], fakeLoadingTimes[i]);
+        }
+    }
+
+    public static void fakeLoading(String[] loadingTextArray) {
+        for (String text : loadingTextArray) {
+            fakeLoading(text, randomTime(), "");
+        }
+    }
+
+    public static void fakeLoading(String[] loadingTextArray, String[] textAndBackgroundColor) {
+        for (int i = 0; i < loadingTextArray.length; i++) {
+            fakeLoading(loadingTextArray[i], randomTime(), textAndBackgroundColor[i]);
+        }
+    }
+
+    private static int randomTime() {
+        //giving it a little bit more tendency to be a bit shorter rather than longer
+        int time1 = rnd.nextInt(10, 300);
+        int time2 = rnd.nextInt(10, 300);
+        int time = (time1 < time2) ? time1 : time2;
+        return time;
+    }
+
+}
