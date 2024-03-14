5d7ed2beac5fe9086fb186ebd7172b89ad1500a6
Niels <46904611+NielsvdWetering@users.noreply.github.com>
2024-03-14 12:59:36 +0100

    Update FakeLoading.java

--- helperclasses/FakeLoading.java
+++ helperclasses/FakeLoading.java
@@ -6,7 +6,7 @@ import java.util.concurrent.TimeUnit;
 public class FakeLoading {
     static Random rnd = new Random();
 
-    public static void fakeLoading(String loadingText, int delayInMilliSeconds, String textAndBackgroundColor) {//1000 milliseconds is 1 second, dus 50 seconden tot 100%
+    public static void fakeLoading(String loadingText, int delayInMilliSeconds, String textAndBackgroundColor) {//1000 milliseconds is 1 second (results in 50 second till 100%)
         String loadingBar = "";
         for (int percent = 0; percent <=100; percent+=2) {
             System.out.printf("\r%s%-21s[%-50s]%s%%%s",textAndBackgroundColor, loadingText, loadingBar, percent, TextColor.RESET);
@@ -24,6 +24,10 @@ public class FakeLoading {
         fakeLoading(loadingText, delayInMilliSeconds, "");
     }
 
+    public static void fakeLoading(String loadingText) {
+        fakeLoading(loadingText, randomTime(), "");
+    }
+
     public static void fakeLoading(String[] loadingTextArray, int[] fakeLoadingTimes, String[] textAndBackgroundColor) {
         for (int i = 0; i < loadingTextArray.length; i++) {
             fakeLoading(loadingTextArray[i], fakeLoadingTimes[i], textAndBackgroundColor[i]);
@@ -38,7 +42,7 @@ public class FakeLoading {
 
     public static void fakeLoading(String[] loadingTextArray) {
         for (String text : loadingTextArray) {
-            fakeLoading(text, randomTime(), "");
+            fakeLoading(text);
         }
     }
 
@@ -52,8 +56,7 @@ public class FakeLoading {
         //giving it a little bit more tendency to be a bit shorter rather than longer
         int time1 = rnd.nextInt(10, 300);
         int time2 = rnd.nextInt(10, 300);
-        int time = (time1 < time2) ? time1 : time2;
-        return time;
+        return Math.min(time1, time2);
     }
 
 }
